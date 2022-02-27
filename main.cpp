52d02fe2ab7fd156efae791e9093ddac17ab8c91
neilchetty <neilchetty4559@gmail.com>
2022-02-28 03:03:40 +0530

    Add Area Of Triangle And Largest, Second Largest, Smallest

--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@
 using namespace std;
 int main()
 {
-    int choice,a,b,temp;
-    float r,area,circum;
+    int choice;
+    float a,b,c,temp,r,area,circum,s1,s2,s3,semi,areatriangle,largest,secondlargest,smallest;
     float const pi=3.14;
     cout<<"1-Replace variable with using third variable"<<endl;
     cout<<"2-Replace variable without using third variable"<<endl;
     cout<<"3-Find Area and circumference of a circle"<<endl;
+    cout<<"4-Find Area of triangle"<<endl;
+    cout<<"5-Find largest, smallest and second largest of three numbers"<<endl;
     cin>>choice;
     switch (choice)
     {
@@ -45,6 +47,33 @@ int main()
         circum=pi*2*r;
         cout<<"Area of the circle = "<<std::setprecision(10)<<area<<endl;
         cout<<"circumference of a circle = "<<std::setprecision(10)<<circum<<endl;
+        break;
+
+    case 4:
+        cout<<"Give three sides of a triangle"<<endl;
+        cin>>s1>>s2>>s3;
+        semi=(s1+s2+s3)/2;
+        areatriangle=sqrt(semi*(semi-s1)*(semi-s2)*(semi-s3));
+        cout<<"Area of triangle is = "<<std::setprecision(10)<<areatriangle;
+        break;
+
+    case 5:
+        cout<<"Enter three numbers"<<endl;
+        cin>>a>>b>>c;
+        largest=a;
+        if(b>largest)
+        largest=b;
+        if(c>largest)
+        largest=c;
+        smallest=a;
+        if(b<smallest)
+        smallest=b;
+        if(c<smallest)
+        smallest=c;
+        secondlargest=(a+b+c)-(largest+smallest);
+        cout<<"Smallest is = "<<smallest<<endl;
+        cout<<"Second largest is = "<<secondlargest<<endl;
+        cout<<"Largest = "<<largest<<endl;
         break;
     default:cout<<"Can't You Read?";
     }
