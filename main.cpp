621840d1e8bcc4c62c8616b8f69398c696d1f79c
neilchetty <neilchetty4559@gmail.com>
2022-02-28 02:49:35 +0530

    Add Area Of A Circle Program And Use Switch

--- main.cpp
+++ main.cpp
@@ -6,11 +6,15 @@ using namespace std;
 int main()
 {
     int choice,a,b,temp;
+    float r,area,circum;
+    float const pi=3.14;
     cout<<"1-Replace variable with using third variable"<<endl;
     cout<<"2-Replace variable without using third variable"<<endl;
+    cout<<"3-Find Area and circumference of a circle"<<endl;
     cin>>choice;
-    if (choice=1)
+    switch (choice)
     {
+    case 1:
         cout<<"Enter the first number"<<endl;
         cin>>a;
         cout<<"Enter the second number"<<endl;
@@ -20,9 +24,9 @@ int main()
         b=a;
         a=temp;
         cout<<"After Interchange A = "<<a<<" And B = "<<b<<endl;
-    }
-    else if (choice=2)
-    {
+        break;
+
+    case 2:
         cout<<"Enter the first number"<<endl;
         cin>>a;
         cout<<"Enter the second number"<<endl;
@@ -32,6 +36,17 @@ int main()
         b=a-b;
         a=a-b;
         cout<<"After Interchange A = "<<a<<" And B = "<<b<<endl;
+        break;
+
+    case 3:
+        cout<<"Enter the radius of a circle: "<<endl;
+        cin>>r;
+        area=pi*r*r;
+        circum=pi*2*r;
+        cout<<"Area of the circle = "<<std::setprecision(10)<<area<<endl;
+        cout<<"circumference of a circle = "<<std::setprecision(10)<<circum<<endl;
+        break;
+    default:cout<<"Can't You Read?";
     }
 return 0;
 }
