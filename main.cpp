55405563464380c7c816f7af9fcc53782b22d22f
neilchetty <neilchetty4559@gmail.com>
2022-02-28 02:25:04 +0530

    Add Changing Variable (b) Program

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
 {
     int choice,a,b,temp;
     cout<<"1-Replace variable with using third variable"<<endl;
+    cout<<"2-Replace variable without using third variable"<<endl;
     cin>>choice;
     if (choice=1)
     {
@@ -18,6 +19,18 @@ int main()
         temp=b;
         b=a;
         a=temp;
+        cout<<"After Interchange A = "<<a<<" And B = "<<b<<endl;
+    }
+    else if (choice=2)
+    {
+        cout<<"Enter the first number"<<endl;
+        cin>>a;
+        cout<<"Enter the second number"<<endl;
+        cin>>b;
+        cout<<"Before Interchange A = "<<a<<" And B = "<<b<<endl;
+        a=a+b;
+        b=a-b;
+        a=a-b;
         cout<<"After Interchange A = "<<a<<" And B = "<<b<<endl;
     }
 return 0;
