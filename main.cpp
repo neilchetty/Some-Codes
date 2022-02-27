a43ad906685fc8a65f3c6b08fa2cfda2a0ff6406
neilchetty <neilchetty4559@gmail.com>
2022-02-28 02:19:44 +0530

    Add Changing Variable (a) Program

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
+#include<conio.h>
+#include<math.h>
+#include<iomanip>
 using namespace std;
 int main()
 {
-    cout<<"Hello World";
-    return 0;
+    int choice,a,b,temp;
+    cout<<"1-Replace variable with using third variable"<<endl;
+    cin>>choice;
+    if (choice=1)
+    {
+        cout<<"Enter the first number"<<endl;
+        cin>>a;
+        cout<<"Enter the second number"<<endl;
+        cin>>b;
+        cout<<"Before Interchange A = "<<a<<" And B = "<<b<<endl;
+        temp=b;
+        b=a;
+        a=temp;
+        cout<<"After Interchange A = "<<a<<" And B = "<<b<<endl;
+    }
+return 0;
 }
